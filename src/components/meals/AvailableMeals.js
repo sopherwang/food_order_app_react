43dbd8c849925a46cc1d3c6fb41fae3f11a7f295
Jiajun Wang <sopherwang@gmail.com>
2021-05-27 21:06:37 -0700

    Create the major UI

--- src/components/meals/AvailableMeals.js
+++ src/components/meals/AvailableMeals.js
@@ -1,4 +1,6 @@
 import classes from './AvailableMeals.module.css'
+import Card from "../common/Card";
+import MealItem from "./MealItem";
 
 
 const DUMMY_MEALS = [
@@ -29,15 +31,16 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealList = DUMMY_MEALS.map(meal => <li>
-    {meal.name}
-  </li>)
+  const mealList = DUMMY_MEALS.map(meal =>
+    <MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price}/>)
 
   return (
     <section className={classes.meals}>
-      <ul>
-        {mealList}
-      </ul>
+      <Card>
+        <ul>
+          {mealList}
+        </ul>
+      </Card>
     </section>
   )
 }
