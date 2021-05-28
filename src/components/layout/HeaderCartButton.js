445d78eac67dee46695cc934fef6b8e3dbba606d
Jiajun Wang <sopherwang@gmail.com>
2021-05-27 22:48:52 -0700

    useEffect to create an animation

--- src/components/layout/HeaderCartButton.js
+++ src/components/layout/HeaderCartButton.js
@@ -1,5 +1,5 @@
 import classes from './HeaderCartButton.module.css'
-import {useContext} from 'react'
+import {useContext, useEffect, useState} from 'react'
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = props => {
@@ -8,7 +8,25 @@ const HeaderCartButton = props => {
     return curNumber + item.amount
   }, 0)
 
-  return <button className={classes.button} onClick={props.onClick}>
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false)
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`
+
+  useEffect(() => {
+    if (ctx.items.length === 0) {
+      return
+    }
+    setBtnIsHighlighted(true)
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false)
+    }, 300)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [ctx.items])
+
+  return <button className={btnClasses} onClick={props.onClick}>
     <span className={classes.icon}>
       <CartIcon/>
     </span>
