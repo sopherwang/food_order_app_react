05a9e9431371ef08ef4630786fea7e7a8a880912
Jiajun Wang <sopherwang@gmail.com>
2021-05-27 21:34:44 -0700

    Handle cart visibility

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import Header from "./components/layout/Header";
 import Meals from "./components/meals/Meals";
 import Cart from "./components/cart/Cart";
+import {useState} from 'react'
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false)
+
+  const showCartHandler = () => {
+    setCartIsShown(true)
+  }
+
+  const hideCartHandler = () => {
+    setCartIsShown(false)
+  }
+
   return (
     <>
-      <Cart/>
-      <Header/>
+      {cartIsShown && <Cart onHideCart={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler}/>
       <main>
         <Meals/>
       </main>
--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -20,7 +20,7 @@ const Cart = (props) => {
       <span>25.99</span>
     </div>
     <div className={classes.actions}>
-      <button className={classes['button--alt']}>Close</button>
+      <button className={classes['button--alt']} onClick={props.onHideCart}>Close</button>
       <button className={classes.button}>Order</button>
     </div>
   </Modal>
