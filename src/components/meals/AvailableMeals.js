44807a3e2c592174e4fa0bfeda3a17f9a9af85db
Jiajun Wang <sopherwang@gmail.com>
2021-05-29 15:25:11 -0700

    Create fetch meal function

--- src/components/meals/AvailableMeals.js
+++ src/components/meals/AvailableMeals.js
@@ -1,45 +1,48 @@
 import classes from './AvailableMeals.module.css'
 import Card from "../common/Card";
 import MealItem from "./MealItem";
-
-
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
+import {useEffect, useState} from "react";
+import useHttp from "../../hooks/use_http";
 
 const AvailableMeals = () => {
-  const mealList = DUMMY_MEALS.map(meal =>
-    <MealItem key={meal.id} id={meal.id} name={meal.name} description={meal.description} price={meal.price}/>)
+  const [meals, setMeals] = useState([])
+  const {isLoading, error, sendRequest: fetchMeals} = useHttp()
+
+  useEffect(() => {
+    const transformMeals = (meal) => {
+      const loadedMeals = [];
+      for (const taskKey in meal) {
+        loadedMeals.push({
+          id: taskKey,
+          name: meal[taskKey].name,
+          description: meal[taskKey].description,
+          price: meal[taskKey].price,
+        });
+      }
+      setMeals(loadedMeals);
+    }
+
+    fetchMeals({
+      url: 'https://react-http-demo-cf6f3-default-rtdb.firebaseio.com/meals.json',
+    }, transformMeals);
+  }, [fetchMeals])
+
+  let content;
+  if (error) {
+    content = <button onClick={fetchMeals}>Try again</button>;
+  } else if (isLoading) {
+    content = 'Loading tasks...';
+  } else {
+    content = <ul>
+      {meals.map(meal =>
+        <MealItem key={meal.id} id={meal.id} name={meal.name} description={meal.description} price={meal.price}/>)}
+    </ul>
+  }
 
   return (
     <section className={classes.meals}>
       <Card>
-        <ul>
-          {mealList}
-        </ul>
+        {content}
       </Card>
     </section>
   )
