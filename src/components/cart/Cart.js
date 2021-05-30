7cbc940e9b43cb402f2179f102b542c23ebc286b
Jiajun Wang <sopherwang@gmail.com>
2021-05-29 17:01:16 -0700

    Implement checkout flow

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,13 +1,17 @@
 import classes from './Cart.module.css'
 import Modal from "../common/Modal";
-import {useContext} from 'react'
+import {useContext, useEffect, useState} from 'react'
 import CartContext from "../../store/cart-context";
 import CartItem from "./CartItem";
+import Checkout from "./Checkout";
+import useHttp from "../../hooks/use_http";
 
 const Cart = (props) => {
   const cartCtx = useContext(CartContext)
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`
   const hasItems = cartCtx.items.length > 0
+
+  const [isCheckout, setIsCheckout] = useState(false)
 
   const cartItemRemoveHandler = id => {
     cartCtx.removeItem(id)
@@ -24,16 +28,38 @@ const Cart = (props) => {
                                         onAdd={cartItemAddHandler.bind(null, cart)}/>)
   }</ul>
 
+  const orderHandler = () => {
+    setIsCheckout(true)
+  }
+
+  const {isLoading, error, sendRequest} = useHttp()
+
+  const submitOrderHandler = async (userData) => {
+    await sendRequest({
+      url: 'https://react-http-demo-cf6f3-default-rtdb.firebaseio.com/order.json',
+      method: 'POST',
+      body: {
+        user: userData,
+        orderedItems: cartCtx.items
+      }
+    }, (data) => {
+    })
+    props.onHideCart()
+  }
+
+  const modalActions = <div className={classes.actions}>
+    <button className={classes['button--alt']} onClick={props.onHideCart}>Close</button>
+    {hasItems && <button className={classes.button} onClick={orderHandler}>Order</button>}
+  </div>
+
   return <Modal onClose={props.onHideCart}>
     {cartItems}
     <div className={classes.total}>
       <span>Total Amount</span>
       <span>{totalAmount}</span>
     </div>
-    <div className={classes.actions}>
-      <button className={classes['button--alt']} onClick={props.onHideCart}>Close</button>
-      {hasItems && <button className={classes.button}>Order</button>}
-    </div>
+    {isCheckout && <Checkout onHideCart={props.onHideCart} onConfirm={submitOrderHandler}/>}
+    {!isCheckout && modalActions}
   </Modal>
 }
 
---
+++ src/components/cart/Checkout.js
@@ -0,0 +1,66 @@
+import classes from './Checkout.module.css'
+import useInput from "../../hooks/use-input";
+
+const Checkout = (props) => {
+  const {
+    value: enteredName,
+    hasError: nameInputIsInvalid,
+    isValid: enteredNameIsValid,
+    valueChangeHandler: nameInputChangeHandler,
+    valueBlurHandler: nameInputBlurHandler,
+    reset: nameReset,
+  } = useInput(value => value.trim().length > 0)
+
+  let formIsValid = false;
+  if (enteredNameIsValid) {
+    formIsValid = true
+  }
+
+  const nameInputClasses = `${classes.control} ${nameInputIsInvalid ? classes.invalid : ''}`
+
+  const confirmHandler = (event) => {
+    event.preventDefault();
+
+    if (!enteredNameIsValid) {
+      return
+    }
+
+    props.onConfirm({
+      name: enteredName,
+    })
+    nameReset()
+  };
+
+  return (
+    <form className={classes.form} onSubmit={confirmHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor='name'>Your Name</label>
+        <input type='text' id='name'
+               value={enteredName}
+               onChange={nameInputChangeHandler}
+               onBlur={nameInputBlurHandler}/>
+        {nameInputIsInvalid && <p className='error-text'>Name must not be empty</p>}
+      </div>
+      <div className={classes.control}>
+        <label htmlFor='street'>Street</label>
+        <input type='text' id='street'/>
+      </div>
+      <div className={classes.control}>
+        <label htmlFor='postal'>Postal Code</label>
+        <input type='text' id='postal'/>
+      </div>
+      <div className={classes.control}>
+        <label htmlFor='city'>City</label>
+        <input type='text' id='city'/>
+      </div>
+      <div className={classes.actions}>
+        <button type='button' onClick={props.onHideCart}>
+          Cancel
+        </button>
+        <button disabled={!formIsValid} className={classes.submit}>Confirm</button>
+      </div>
+    </form>
+  );
+};
+
+export default Checkout;
